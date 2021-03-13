b6c966ff826978e4a69b38c628e2cbab80cd5fca
Peterzhang123 <peterzzq1986@gmail.com>
2021-03-13 11:35:13 -0800

    adding example constructor ,new object and stopwatch

---
+++ javascript-constructors/example-constructor.js
@@ -0,0 +1,11 @@
+
+function ExampleConstructor() {
+  
+}
+
+console.log("typeof ExampleConstructor: ",typeof ExampleConstructor);
+
+var aExampleConstructor = new ExampleConstructor();
+
+console.log("instanceof aExampleConstructor: ", aExampleConstructor instanceof ExampleConstructor);
+
